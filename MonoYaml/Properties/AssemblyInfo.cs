b00f24ee8844a72d1a1db3bf3ffe955f5286b6ed
jlyonsmith <john@lyon-smith.org>
2012-11-17 16:09:05 -0800

    Replacing implementation with one from CodePlex.

--- MonoYaml/Properties/AssemblyInfo.cs
+++ MonoYaml/Properties/AssemblyInfo.cs
@@ -2,13 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("MonoYaml")]
-[assembly: AssemblyDescription("MonoYaml")]
+[assembly: AssemblyTitle("YamlSerializer")]
+[assembly: AssemblyDescription("This library serializes arbitrary .NET native objects into YAML text. It can also manipulates a generic YAML documents.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Osamu TAKEUCHI <osamu@big.jp>")]
+[assembly: AssemblyProduct("YamlSerializer")]
+[assembly: AssemblyCopyright("Copyright ©  2009 Osamu TAKEUCHI <osamu@big.jp>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.9.0.2")]
+[assembly: AssemblyFileVersion("0.9.0.2")]
+[assembly: InternalsVisibleTo("MonoYamlTests, PublicKey=0024000004800000940000000602000000240000525341310004000011000000d5fc67971ea115e6e8b5a4805a854de36f957545de73ebcb281c51634b914db05903fdc54efd6242f18178796b4b64a47dce793c60bb51d59e2f0125b1b9631407ab3366703345ce9e3f37048596e17212b5b36019cab9ff25e1abfc2cda48185c3ef4baf9a403893e9d97053d2222549e439fd9731c16c8d06280b8df93ee80")]
---
+++ MonoYamlTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YamlSerializerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YamlSerializerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
